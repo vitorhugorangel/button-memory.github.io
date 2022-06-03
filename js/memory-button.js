5799aa76b35d955b9abaf9764b2b5925f9b4cc3b
vitorhugorangel <vitorhugo.rangel@yahoo.com.br>
2022-06-03 18:54:24 -0300

    Trocando nome de exibição

--- js/memory-button.js
+++ js/memory-button.js
@@ -11,7 +11,7 @@ memoryButton = {
         fail: 'fail.mp3',
         complete: 'complete.mp3',
         combinations: ['0.mp3', '1.mp3', '2.mp3', '3.mp3', '4.mp3', '5.mp3', '6.mp3', '7.mp3', '8.mp3'],
-        
+
         loadAudio(filename) {
 
             const file = `./audio/${filename}?cb=${new Date().getTime()}`
@@ -23,15 +23,15 @@ memoryButton = {
 
         loadAudios() {
 
-            if (typeof(memoryButton.audio.start) == "object") return
+            if (typeof (memoryButton.audio.start) == "object") return
 
             memoryButton.audio.start = memoryButton.audio.loadAudio(memoryButton.audio.start)
             memoryButton.audio.complete = memoryButton.audio.loadAudio(memoryButton.audio.complete)
             memoryButton.audio.fail = memoryButton.audio.loadAudio(memoryButton.audio.fail)
-            memoryButton.audio.combinations = memoryButton.audio.combinations.map ( (audio) => memoryButton.audio.loadAudio(audio))
+            memoryButton.audio.combinations = memoryButton.audio.combinations.map((audio) => memoryButton.audio.loadAudio(audio))
 
         }
-        
+
 
     },
     interface: {
@@ -47,7 +47,7 @@ memoryButton = {
         },
 
         turnAllLedsOff() {
-            
+
             const computerLedPanel = memoryButton.interface.computerLedPanel
             const playerLedPanel = memoryButton.interface.playerLedPanel
 
@@ -63,7 +63,7 @@ memoryButton = {
         },
 
         playItem(index, combinationPosition, location = 'computer') {
-            
+
             const leds = (location == 'computer') ? memoryButton.interface.computerLedPanel : memoryButton.interface.playerLedPanel
             const memPanel = memoryButton.interface.memoryPanel.children[index]
 
@@ -77,7 +77,7 @@ memoryButton = {
         },
 
         endGame(type = "fail") {
-            
+
             const memPanel = memoryButton.interface.memoryPanel
             const ledPanel = memoryButton.interface.computerLedPanel
             const audio = (type == "complete") ? memoryButton.audio.complete : memoryButton.audio.fail
@@ -98,12 +98,12 @@ memoryButton = {
                 }
                 setTimeout(() => {
                     for (var i = 0; i < memPanel.children.length; i++) {
-                    if (memPanel.children[i].tagName == "DIV")
-                        memPanel.children[i].classList.remove(typeClasses[0])
+                        if (memPanel.children[i].tagName == "DIV")
+                            memPanel.children[i].classList.remove(typeClasses[0])
                     }
                     for (var i = 0; i < ledPanel.children.length; i++) {
-                    if (ledPanel.children[i].tagName == "DIV")
-                        ledPanel.children[i].classList.remove(typeClasses[1])
+                        if (ledPanel.children[i].tagName == "DIV")
+                            ledPanel.children[i].classList.remove(typeClasses[1])
                     }
                 }, 900);
 
@@ -123,18 +123,18 @@ memoryButton = {
 
         },
 
-        disableButtons() { 
+        disableButtons() {
 
             const playerMemory = memoryButton.interface.playerMemory
             playerMemory.classList.remove('playerActive')
 
             for (var i = 0; i < playerMemory.children.length; i++) {
-            if (playerMemory.children[i].tagName == "DIV")
-                playerMemory.children[i].classList.remove("playerMemoryActive");
+                if (playerMemory.children[i].tagName == "DIV")
+                    playerMemory.children[i].classList.remove("playerMemoryActive");
             }
 
         },
-        
+
 
     },
 
@@ -143,26 +143,26 @@ memoryButton = {
             console.log("Loading Game...")
             memoryButton.audio.loadAudios()
 
-            const playerMemory  = memoryButton.interface.playerMemory
+            const playerMemory = memoryButton.interface.playerMemory
             const memory = memoryButton.interface.playerMemoryButtons
-            
+
             Array.prototype.forEach.call(memory, (element) => {
 
                 element.addEventListener("click", () => {
-                if (playerMemory.classList.contains("playerActive")) {
-                    memoryButton.play(parseInt(element.dataset.memory))
-                    console.log("O valor do elemento clicado é: " + element.dataset.memory)
+                    if (playerMemory.classList.contains("playerActive")) {
+                        memoryButton.play(parseInt(element.dataset.memory))
+                        console.log("O valor do elemento clicado é: " + element.dataset.memory)
 
-                    element.style.animation = "playermemoryClick .4s"
-                    setTimeout(() => element.style.animation = "", 400)
-                }
-                })  
+                        element.style.animation = "playermemoryClick .4s"
+                        setTimeout(() => element.style.animation = "", 400)
+                    }
+                })
 
             })
         })
 
 
-     },
+    },
     start() {
 
         memoryButton.computerCombination = memoryButton.createCombination()
@@ -175,13 +175,13 @@ memoryButton = {
         })
 
     },
-    
+
     createCombination() {
 
         let newCombination = []
-        for (let n = 0; n < memoryButton.combinationMaxPosition; n++){
+        for (let n = 0; n < memoryButton.combinationMaxPosition; n++) {
             const position = Math.floor((Math.random() * memoryButton.memoryMaxCombination) + 1)
-            newCombination.push(position-1)
+            newCombination.push(position - 1)
         }
         return newCombination
 
@@ -193,7 +193,7 @@ memoryButton = {
         memoryButton.playerCombination.push(index)
 
         if (memoryButton.isTheRightCombination(memoryButton.playerCombination.length)) {
-            
+
             if (memoryButton.playerCombination.length == memoryButton.combinationMaxPosition) {
                 memoryButton.interface.endGame("complete")
                 setTimeout(() => {
@@ -205,7 +205,7 @@ memoryButton = {
             if (memoryButton.playerCombination.length == memoryButton.computerCombinationPosition) {
                 memoryButton.computerCombinationPosition++
                 setTimeout(() => {
-                        memoryButton.playCombination()
+                    memoryButton.playCombination()
                 }, 1200)
                 return
             }
@@ -215,7 +215,7 @@ memoryButton = {
             memoryButton.interface.endGame("fail")
             document.getElementById("title").textContent = "Você é o impostor"
             setTimeout(() => {
-                document.getElementById("title").textContent = "START REACTOR"
+                document.getElementById("title").textContent = "MEMORY BUTTON GAME"
                 memoryButton.start()
             }, 1400)
             return
@@ -228,11 +228,11 @@ memoryButton = {
         memoryButton.interface.disableButtons()
         memoryButton.interface.turnAllLedsOff()
 
-        for (let i = 0; i <= memoryButton.computerCombinationPosition - 1; i++){
+        for (let i = 0; i <= memoryButton.computerCombinationPosition - 1; i++) {
 
             setTimeout(() => {
                 memoryButton.interface.playItem(memoryButton.computerCombination[i], i)
-            }, 400 * (i+1))
+            }, 400 * (i + 1))
         }
 
         setTimeout(() => {
@@ -240,12 +240,12 @@ memoryButton = {
             memoryButton.interface.enableButtons()
         }, 600 * memoryButton.computerCombinationPosition)
 
-     },
-    
+    },
+
     isTheRightCombination(position) {
-        
+
         let computerCombination = memoryButton.computerCombination.slice(0, position)
-        return ( computerCombination.toString() == memoryButton.playerCombination.toString())
+        return (computerCombination.toString() == memoryButton.playerCombination.toString())
 
     },
 
